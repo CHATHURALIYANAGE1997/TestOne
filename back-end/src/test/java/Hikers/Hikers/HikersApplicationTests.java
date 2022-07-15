8252937dfb2fb6bf035795397d917956a9d4fba4
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-07-15 11:31:41 +0530

    Add files via upload

---
+++ back-end/src/test/java/Hikers/Hikers/HikersApplicationTests.java
@@ -0,0 +1,13 @@
+package Hikers.Hikers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HikersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
